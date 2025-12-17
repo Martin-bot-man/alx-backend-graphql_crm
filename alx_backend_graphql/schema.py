a2be031a4ccec52de3add5a5d2c78e1347307911
Martin-bot-man <martinwere14@gmail.com>
2025-12-17 06:42:12 +0300

    feat: created schema.py

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,9 +1,14 @@
 import graphene
-
+from crm.schema import CRMQuery
 class Query(graphene.ObjectType):
     hello = graphene.String()
+    crm = graphene.Field(CRMQuery)
 
     def resolve_hello(self, info):
-        return 'Hello World'
-    
-schema = graphene.Schema(query = Query)    
+        return "Hello, GraphQL!"
+
+    def resolve_crm(self, info):
+        return CRMQuery()
+
+
+schema = graphene.Schema(query=Query) 
